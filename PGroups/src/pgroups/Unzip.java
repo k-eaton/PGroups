df56ab2a3d185685553ecf8070694b391f0c730e
k-eaton <katrina.eaton@gmail.com>
2017-09-29 15:15:25 -0700

    start to move unzip and extractFile to Unzip class

--- PGroups/src/pgroups/Unzip.java
+++ PGroups/src/pgroups/Unzip.java
@@ -5,20 +5,79 @@
  */
 package pgroups;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-/**
- *
+/*
  * @author Katrina Eaton & Melinda Perez
  */
 public class Unzip {
     
+    public String destDirectory;
+    public File destDir;
+    public String filePath;
+    public ZipEntry entry;
+    public ZipInputStream zipIn;
+    
+    public final int BUFFER_SIZE = 4096;
+    
+    public Unzip(String zipFilePath, String desDirectory) throws IOException {
+        try {
+            destDirectory = desDirectory;
+            destDir = new File(desDirectory);
+            if (!destDir.exists()) {
+                destDir.mkdir();
+            }
+            zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+            
+            // iterates over entries in the zip file
+            entry = zipIn.getNextEntry();
+            filePath = destDirectory + File.separator + entry.getName();
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+    
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+    
+    public void unzipTheFile() throws IOException {
+        try {
+            while (entry != null) {
+                filePath = destDirectory + File.separator + entry.getName();
+
+                if (!entry.isDirectory()) {
+                    // if the entry is a file, extracts it
+                    extractFile(zipIn, filePath);
+                } else {
+                    // if the entry is a directory, make the directory
+                    File dir = new File(filePath);
+                    dir.mkdir();
+                }
+                zipIn.closeEntry();
+                entry = zipIn.getNextEntry();
+            }
+            zipIn.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+    
 }
-//public void unzip(String zipFilePath, String destDirectory) throws IOException {
+//public void unzip(String zipfilePath, String destDirectory) throws IOException {
 //        File destDir = new File(destDirectory);
 //        if (!destDir.exists()) {
 //            destDir.mkdir();
